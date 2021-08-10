3f6cd5c7a146367e0e9dbe667cf896012f470b7b
Vitaliy Potapov <noginsk@rambler.ru>
2021-08-10 17:25:45 +0300

    rename http -> server

--- src/proxy/ws.ts
+++ src/proxy/ws.ts
@@ -3,7 +3,7 @@
  */
 import { once } from 'events';
 import { IUtf8Message } from 'websocket';
-import { server } from '../http/server';
+import { server } from '../server';
 import AbortController from 'abort-controller';
 
 let ac: AbortController;
